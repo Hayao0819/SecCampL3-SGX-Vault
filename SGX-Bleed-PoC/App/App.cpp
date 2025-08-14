45675988025bccb27b69f9e336e788fb3aa2613a
hayao <shun819.mail@gmail.com>
2025-08-14 06:07:14 +0000

    feat: Enclave初期化関数を改善し、スイッチレスコールを有効化

--- SGX-Bleed-PoC/App/App.cpp
+++ SGX-Bleed-PoC/App/App.cpp
@@ -1,177 +1,101 @@
+#include <sgx_urts.h>
+#include <sgx_uswitchless.h>
+
 #include <cstdio>
 #include <cstring>
 #include <iostream>
+
 #include "Enclave_u.h"
-#include <sgx_urts.h>
 #include "error_print.h"
-
-
 
 sgx_enclave_id_t global_eid = 0;
 
-
-
 /* OCALL implementations */
-void ocall_print(const char* str)
-{
-	std::cout << "Output from OCALL: " << std::endl;
-	std::cout << str << std::endl;
-	
-	return;
+void ocall_print(const char* str) {
+    std::cout << "Output from OCALL: " << std::endl;
+    std::cout << str << std::endl;
+
+    return;
 }
-
-
 
 /* Enclave initialization function */
-int initialize_enclave()
-{
-	std::string launch_token_path = "enclave.token";
-	std::string enclave_name = "enclave.signed.so";
-	const char* token_path = launch_token_path.c_str();
+int initialize_enclave(sgx_enclave_id_t& eid) {
+    /* LEはDeprecatedになったので、起動トークンはダミーで代用する */
+    sgx_launch_token_t token = {0};
 
-	sgx_launch_token_t token = {0};
-	sgx_status_t status = SGX_ERROR_UNEXPECTED;
-	int updated = 0;
+    /* 起動トークンが更新されているかのフラグ。Deprecated。 */
+    int updated = 0;
 
+    /* 署名済みEnclaveイメージファイル名 */
+    std::string enclave_image_name = "enclave.signed.so";
 
-	/*==============================================================*
-	 * Step 1: Obtain enclave launch token                          *
-	 *==============================================================*/
-	
-	/* If exist, load the enclave launch token */
-	FILE *fp = fopen(token_path, "rb");
+    sgx_status_t status;
 
-	/* If token doesn't exist, create the token */
-	if(fp == NULL && (fp = fopen(token_path, "wb")) == NULL)
-	{		
-		/* Storing token is not necessary, so file I/O errors here
-		 * is not fatal
-		 */
-		std::cerr << "Warning: Failed to create/open the launch token file ";
-		std::cerr << "\"" << launch_token_path << "\"." << std::endl;
-	}
+    sgx_uswitchless_config_t us_config = SGX_USWITCHLESS_CONFIG_INITIALIZER;
+    void* enclave_ex_p[32] = {0};
 
+    enclave_ex_p[SGX_CREATE_ENCLAVE_EX_SWITCHLESS_BIT_IDX] = &us_config;
 
-	if(fp != NULL)
-	{
-		/* read the token from saved file */
-		size_t read_num = fread(token, 1, sizeof(sgx_launch_token_t), fp);
+    /*
+     * Switchless Callが有効化されたEnclaveの作成。
+     * NULLの部分はEnclaveの属性（sgx_misc_attribute_t）が入る部分であるが、
+     * 不要かつ省略可能なのでNULLで省略している。
+     */
+    status = sgx_create_enclave_ex(enclave_image_name.c_str(), SGX_DEBUG_FLAG,
+                                   &token, &updated, &eid, NULL, SGX_CREATE_ENCLAVE_EX_SWITCHLESS,
+                                   (const void**)enclave_ex_p);
 
-		/* if token is invalid, clear the buffer */
-		if(read_num != 0 && read_num != sizeof(sgx_launch_token_t))
-		{
-			memset(&token, 0x0, sizeof(sgx_launch_token_t));
+    if (status != SGX_SUCCESS) {
+        /* error_print.cppで定義 */
+        sgx_error_print(status);
+        return -1;
+    }
 
-			/* As aforementioned, if token doesn't exist or is corrupted,
-			 * zero-flushed new token will be used for launch.
-			 * So token error is not fatal.
-			 */
-			std::cerr << "Warning: Invalid launch token read from ";
-			std::cerr << "\"" << launch_token_path << "\"." << std::endl;
-		}
-	}
-
-
-	/*==============================================================*
-	 * Step 2: Initialize enclave by calling sgx_create_enclave     *
-	 *==============================================================*/
-
-	status = sgx_create_enclave(enclave_name.c_str(), SGX_DEBUG_FLAG, &token,
-		&updated, &global_eid, NULL);
-	
-	if(status != SGX_SUCCESS)
-	{
-		/* Defined at error_print.cpp */
-		sgx_error_print(status);
-		
-		if(fp != NULL)
-		{
-			fclose(fp);
-		}
-
-		return -1;
-	}
-
-	/*==============================================================*
-	 * Step 3: Save the launch token if it is updated               *
-	 *==============================================================*/
-	
-	/* If there is no update with token, skip save */
-	if(updated == 0 || fp == NULL)
-	{
-		if(fp != NULL)
-		{
-			fclose(fp);
-		}
-
-		return 0;
-	}
-
-
-	/* reopen with write mode and save token */
-	fp = freopen(token_path, "wb", fp);
-	if(fp == NULL) return 0;
-
-	size_t write_num = fwrite(token, 1, sizeof(sgx_launch_token_t), fp);
-
-	if(write_num != sizeof(sgx_launch_token_t))
-	{
-		std::cerr << "Warning: Failed to save launch token to ";
-		std::cerr << "\"" << launch_token_path << "\"." << std::endl;
-	}
-
-	fclose(fp);
-
-	return 0;
+    return 0;
 }
 
+int main_logic() {
+    sgx_status_t status = ecall_test(global_eid, &retval,
+                                     message, message_len);
 
+    if (status != SGX_SUCCESS) {
+        sgx_error_print(status);
 
+        return -1;
+    } else {
+        /* This function also can display succeeded message */
+        sgx_error_print(status);
+    }
+}
 
-int main()
-{
-	/* initialize enclave */
-	if(initialize_enclave() < 0)
-	{
-		std::cerr << "App: fatal error: Failed to initialize enclave.";
-		std::cerr << std::endl;
-		return -1;
-	}
+int main() {
+    sgx_enclave_id_t eid = -1;
 
+    /* initialize enclave */
+    if (initialize_enclave(eid) < 0) {
+        std::cerr << "App: fatal error: Failed to initialize enclave.";
+        std::cerr << std::endl;
+        return -1;
+    }
 
-	/* start ECALL */
-	const char *message = "Hello Enclave.";
-	size_t message_len = strlen(message);
-	int retval = -9999;
+    /* start ECALL */
+    const char* message = "Hello Enclave.";
+    size_t message_len = strlen(message);
+    int retval = -9999;
 
-	std::cout << "Execute ECALL.\n" << std::endl;
+    std::cout << "Execute ECALL.\n"
+              << std::endl;
 
-	sgx_status_t status = ecall_test(global_eid, &retval,
-		message, message_len);
+    main_logic();
 
-	if(status != SGX_SUCCESS)
-	{
-		sgx_error_print(status);
+    /* print ECALL result */
+    std::cout << "\nReturned integer from ECALL is: " << retval << std::endl;
+    std::cout << std::endl;
 
-		return -1;
-	}
-	else
-	{
-		/* This function also can display succeeded message */
-		sgx_error_print(status);
-	}
+    /* Destruct the enclave */
+    sgx_destroy_enclave(global_eid);
 
+    std::cout << "Whole operations have been executed correctly." << std::endl;
 
-	/* print ECALL result */
-	std::cout << "\nReturned integer from ECALL is: " << retval << std::endl;
-	std::cout << std::endl;
-
-
-
-	/* Destruct the enclave */
-	sgx_destroy_enclave(global_eid);
-
-	std::cout << "Whole operations have been executed correctly." << std::endl;
-
-	return 0;
+    return 0;
 }
